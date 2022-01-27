e9283a7d3f28290ab5d35edeae233ce32c10e9c2
MarkoMoist <89842536+MarkoMoist@users.noreply.github.com>
2022-01-27 20:37:17 +0200

    Add files via upload

---
+++ BitCoinCalculator/BitCoinCalculator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitCoinCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getRatesBtn_Click(object sender, EventArgs e)
+        {
+            if(currencyMenu.SelectedItem.ToString() == "EUR")
+            {
+                resultLbl.Visible = true;
+                result.Visible = true;
+
+                BitCoinRates resultRates = getRates();
+                int userCoins = Int32.Parse(amountOfBtc.Text);//the mount of user`s bitcoins
+
+                float currentRate = resultRates.bpi.EUR.rate_float;// current bitcoin rate in EUR
+                float btcResult = userCoins * currentRate;
+                result.Text = $"{btcResult} {resultRates.bpl.EUR.code}";
+            }
+        }
+
+        private BitCoinRates getRates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static BitCoinRates GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            BitCoinRates bitcoin;
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitCoinRates>(response);
+            }
+            return bitcoin; 
+        }
+    }
+}
